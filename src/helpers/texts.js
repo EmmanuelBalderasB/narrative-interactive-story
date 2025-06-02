c1d31e3cc5ca5a036c9b409e5424730693f2eafc
Emmanuel <ebalderasbeq@gmail.com>
2025-06-02 02:31:47 -0600

    verse texts

---
+++ src/helpers/texts.js
@@ -0,0 +1,46 @@
+const verse1 = `It's been 8 months since I last saw them…
+8 months of dread, not knowing…
+8 months of guilt. the pain.
+Could things be different now if I had stayed? 
+`
+const verse2 = `It's these thoughts that keep me awake.
+Away from the things I crave,
+Peace, serenity.
+Instead, these are all full of guilt. 
+`
+const verse3 = `All the mistakes I made,
+All the decisions I took,
+All led me here. To this void, this God forsaken place.
+`
+
+const verse4 = `I still hear your voices,
+your arguments,
+your complaints,
+your ideas,
+your joy,
+your brilliance…
+All shut away now into void. Can't help but feel like I am the one to blame.
+`
+const verse5 = `This fate,
+this end that you did not deserve,
+will now be a product of it, my shame. 
+My anguish.
+Wish i could've done something more,
+something different.`
+
+const verse6 = `As for me,
+While I roam this desolate place,
+I attempt to put back together the pieces of me that I left with you.
+Now, they're all scrambled around the wreckage that's left`
+
+const verse7 = `Maybe this is what was meant to be…
+Alone, 
+fragmented, 
+I now roam. 
+In search to find what's left of you,
+What's left of my humanity,
+Alone.`
+
+const verses = [verse1, verse2, verse3, verse4, verse5, verse6, verse7]
+
+export default verses
